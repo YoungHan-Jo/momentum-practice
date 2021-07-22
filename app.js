7bd938f41ed8b8539f6931d986bb9ffab73906f9
JYH <verin4494@gmail.com>
2021-07-22 22:02:44 +0900

    0722

---
+++ app.js
@@ -0,0 +1,14 @@
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+console.log(daysOfWeek);
+
+daysOfWeek.push("holiday");
+
+console.log(daysOfWeek);
+
+function sayHello(name) {
+  console.log("Hello my name is " + name);
+}
+
+sayHello("Nico");
+sayHello("Jo");
